6ef1dfb9482f9e7e9cc8e060c73c2cd059c7c6d9
Dalraj Bains <dbains38@my.bcit.ca>
2024-03-02 23:15:38 -0800

    (moved navbar code all into navbar.html, moved some code for visual clarity,js scripts declared and renamed navbar/footer)

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,5 +1,35 @@
 function loadSkeleton() {
-  console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+  console.log($('#navbarPlaceholder').load('./text/navbar.html'));
   console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.getElementById('burgerMenu');
+  const menu = document.getElementById('menu');
+
+  burgerMenu.addEventListener('click', function (event) {
+      event.stopPropagation(); // Prevent the click event from propagating to the document body
+      toggleMenu();
+  });
+
+  document.body.addEventListener('click', function () {
+      if (!menu.classList.contains('hidden')) {
+          toggleMenu();
+      }
+  });
+
+  // Prevent clicks inside the menu from closing it
+  menu.addEventListener('click', function (event) {
+      event.stopPropagation();
+  });
+
+  function toggleMenu() {
+      menu.classList.toggle('hidden');
+      if (menu.classList.contains('hidden')) {
+          menu.style.right = '-250px';
+      } else {
+          menu.style.right = '0';
+      }
+  }
+});
