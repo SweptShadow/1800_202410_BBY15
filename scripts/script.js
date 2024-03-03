3554731a8d37482b44b4f9bb81f7e63a9cea5bb5
Daniil Yarygin <dyarygin@my.bcit.ca>
2024-03-02 21:15:45 -0800

    (added main landing page and modified script)

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,29 @@
-function sayHello() {
-    
-}
-//sayHello();
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = document.getElementById('burgerMenu');
+    const menu = document.getElementById('menu');
+
+    burgerMenu.addEventListener('click', function (event) {
+        event.stopPropagation(); // Prevent the click event from propagating to the document body
+        toggleMenu();
+    });
+
+    document.body.addEventListener('click', function () {
+        if (!menu.classList.contains('hidden')) {
+            toggleMenu();
+        }
+    });
+
+    // Prevent clicks inside the menu from closing it
+    menu.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    function toggleMenu() {
+        menu.classList.toggle('hidden');
+        if (menu.classList.contains('hidden')) {
+            menu.style.right = '-250px';
+        } else {
+            menu.style.right = '0';
+        }
+    }
+});
