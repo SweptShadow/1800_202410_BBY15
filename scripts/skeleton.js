4198792ff3d8930c66cbdaedd8666ed236903494
Dalraj Bains <dbains38@my.bcit.ca>
2024-03-14 19:08:02 -0700

    (added footer navbar and javascript for it to load with placeholder)

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,16 +1,34 @@
 function loadSkeleton() {
-  $('#navbarPlaceholder').load('./text/navbar.html', function() {
+  $('#navbarPlaceholder').load('./text/navbar.html', function () {
     const burgerMenu = document.getElementById('burgerMenu');
     const menu = document.getElementById('navLinks');
 
     // Ensure correct initial state
     if (!menu.classList.contains('menu-hidden')) {
-        menu.classList.add('menu-hidden');
+      menu.classList.add('menu-hidden');
     }
     burgerMenu.addEventListener('click', function () {
-        menu.classList.toggle('menu-hidden');
+      menu.classList.toggle('menu-hidden');
     });
   });
-  $('#footerPlaceholder').load('./text/footer.html');
+
+  $('#footerPlaceholder').load('./text/footer.html', function () {
+    function homeFunction() {
+      alert("Home button clicked");
+    }
+
+    function routesFunction() {
+      alert("Routes button clicked");
+    }
+
+    function returnFunction() {
+      alert("Return button clicked");
+    }
+
+    // Attach the functions to window objects so they can be accessed globally
+    window.homeFunction = homeFunction;
+    window.routesFunction = routesFunction;
+    window.returnFunction = returnFunction;
+  });
 }
 loadSkeleton();
