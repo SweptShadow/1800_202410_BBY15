ec7d3b08e642722b16ef74fcd9a7ab80bd40c573
Dalraj Bains <dbains38@my.bcit.ca>
2024-04-04 14:28:00 -0700

    (changing api key because someone started messing with our database)

--- scripts/busroutes.js
+++ scripts/busroutes.js
@@ -1,22 +1,4 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
-var firebaseConfig = {
-  apiKey: "AIzaSyBXJ7xXN8R3_4lFKMMG6Lr2ObdoQi5sSyU",
-  authDomain: "bus-w-us.firebaseapp.com",
-  projectId: "bus-w-us",
-  storageBucket: "bus-w-us.appspot.com",
-  messagingSenderId: "1019919123496",
-  appId: "1:1019919123496:web:6c9d4776d24644723c211d"
-};
-
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 //Array of bus data fields and corresponding element IDs
 var busDataFields = ['bus25Data', 'bus110Data', 'bus122Data', 'bus130Data', 'bus222Data'];
 
