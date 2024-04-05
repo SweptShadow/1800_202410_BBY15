9f27b3e4f88a42885da229d903941a88a3ec7b02
Dalraj Bains <dbains38@my.bcit.ca>
2024-04-05 10:52:39 -0700

    (review page, js, thanks.html added; landing style error fix)

---
+++ scripts/review.js
@@ -0,0 +1,54 @@
+var routeDocID = localStorage.getItem("routeDocID");    //visible to all functions on this page
+
+function getRouteName(id) {
+  db.collection("routes")
+    .doc(id)
+    .get()
+    .then((thisRoute) => {
+      var routeName = thisRoute.data().name;
+      document.getElementById("routeName").innerHTML = routeName;
+    });
+}
+
+getRouteName(routeDocID);
+
+//Add this JavaScript code to make stars clickable
+//Select all elements with the class name "star" and store them in the "stars" variable
+const stars = document.querySelectorAll('.star');
+stars.forEach((star, index) => {
+  star.addEventListener('click', () => {
+    for (let i = 0; i <= index; i++) {
+      document.getElementById(`star${i + 1}`).textContent = 'star';
+    }
+  });
+});
+
+function writeReview() {
+  console.log("inside write review");
+  let routeBusy = document.getElementById("busy").value;
+  let routeRecommend = document.querySelector('input[name="recommend"]:checked').value;
+  let reviewTime = document.getElementById("time").value;
+
+  console.log(routeBusy, routeRecommend, reviewTime, routeRating);
+
+  var user = firebase.auth().currentUser;
+  if (user) {
+    var currentUser = db.collection("users").doc(user.uid);
+    var userID = user.uid;
+
+    db.collection("reviews").add({
+      routeDocID: routeDocID,
+      userID: userID,
+      busy: routeBusy,
+      recommend: routeRecommend,
+      time: reviewTime,
+      rating: routeRating,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }).then(() => {
+      window.location.href = "thanks.html";
+    });
+  } else {
+    console.log("No user is signed in");
+    window.location.href = 'review.html';
+  }
+}
